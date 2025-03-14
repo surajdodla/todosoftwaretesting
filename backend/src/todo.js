4fa8afb073852ac4cf12e5c823479f9f3742fa25
Gubala Haribabu Laasya Sai <lagu23@student.bth.se>
2025-03-14 16:47:54 +0100

    addtodo

--- backend/src/todo.js
+++ backend/src/todo.js
@@ -4,22 +4,8 @@ function addTodo(todos, newTodo) {
     return [...todos, newTodo];
 }
 
-function deleteTodo(todos, id) {
-    return todos.filter(todo => todo.id !== id);
-}
 
-function updateTodo(todos, id, updatedText) {
-    return todos.map(todo =>
-        todo.id === id ? { ...todo, text: updatedText } : todo
-    );
-}
-
-function getTodos(todos) {
-    return todos;
-}
 
 module.exports = { addTodo, 
-    deleteTodo, 
-    updateTodo, 
-    getTodos 
+    
 };
